311939310f55e68c1e557c7d901fd8f0f6648066
Veronika L <verokipri@yahoo.com>
2022-10-14 18:23:41 +0200

    data done

--- lib/core/error/failure.dart
+++ lib/core/error/failure.dart
@@ -6,6 +6,6 @@ abstract class Failure extends Equatable{
   List<Object> get props => [];
 }
 
-class ServerFailur extends Failure{}
+class ServerFailure extends Failure{}
 
-class CacheFailur extends Failure{}
+class CacheFailure extends Failure{}
--- lib/feature/data/repositories/person_repository_impl.dart
+++ lib/feature/data/repositories/person_repository_impl.dart
@@ -1,19 +1,57 @@
+import 'package:randm/core/platform/network_info.dart';
+import 'package:randm/feature/data/datasources/person_local_data_source.dart';
+import 'package:randm/feature/data/datasources/person_remote_data_source.dart';
 import 'package:randm/feature/domain/entities/person_entity.dart';
 import 'package:randm/core/error/failure.dart';
 import 'package:dartz/dartz.dart';
 import 'package:randm/feature/domain/repository/person_repository.dart';
 
+import '../../../core/error/exception.dart';
+import '../models/person_model.dart';
+
 class PersonRepositoryImpl implements PersonRepository{
+  final PersonRemoteDataSource remoteDataSource;
+  final PersonLocalDataSource localDataSource;
+  final NetworkInfo networkInfo;
+
+  PersonRepositoryImpl({
+    required this.remoteDataSource, 
+    required this.localDataSource, 
+    required this.networkInfo});
+
   @override
-  Future<Either<Failure, List<PersonEntity>>> getAllPersons(int page) {
-    // TODO: implement getAllPersons
-    throw UnimplementedError();
+  Future<Either<Failure, List<PersonEntity>>> getAllPersons(int page) async {
+    return await _getPersons(() {
+      return remoteDataSource.getAllPersons(page);
+    });
   }
 
   @override
-  Future<Either<Failure, List<PersonEntity>>> searchPerson(String query) {
-    // TODO: implement searchPerson
-    throw UnimplementedError();
+  Future<Either<Failure, List<PersonEntity>>> searchPerson(String query) async{
+    return await _getPersons(() {
+      return remoteDataSource.searchPerson(query);
+    });
+  }
+
+
+  Future<Either<Failure, List<PersonModel>>> _getPersons(
+      Future<List<PersonModel>> Function() getPersons) async {
+    if (await networkInfo.isConnected) {
+      try {
+        final remotePerson = await getPersons();
+        localDataSource.personToCache(remotePerson);
+        return Right(remotePerson);
+      } on ServerException {
+        return Left(ServerFailure());
+      }
+    } else {
+      try {
+        final localPerson = await localDataSource.getLastPersonsFromCache();
+        return Right(localPerson);
+      } on CacheException {
+        return Left(CacheFailure());
+      }
+    }
   }
 
 }
