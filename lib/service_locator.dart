8ec7f00b465c2bade0a9cc5efeb6847b2fa7755c
Veronika L <verokipri@yahoo.com>
2022-10-15 14:35:55 +0200

    sevice_locator (DI)

---
+++ lib/service_locator.dart
@@ -0,0 +1,63 @@
+import 'package:get_it/get_it.dart';
+import 'package:http/http.dart' as http;
+import 'package:internet_connection_checker/internet_connection_checker.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'core/platform/network_info.dart';
+import 'feature/data/datasources/person_local_data_source.dart';
+import 'feature/data/datasources/person_remote_data_source.dart';
+import 'feature/data/repositories/person_repository_impl.dart';
+import 'feature/domain/repository/person_repository.dart';
+import 'feature/domain/usercases/get_all_persons.dart';
+import 'feature/domain/usercases/search_person.dart';
+import 'feature/presentation/bloc/person_list_cubit/person_list_cubit.dart';
+import 'feature/presentation/bloc/search_bloc/search_bloc.dart';
+ 
+ 
+ 
+final sl = GetIt.instance;
+
+Future<void> init() async {
+  // BLoC / Cubit
+  sl.registerFactory(
+    () => PersonListCubit(getAllPersons: sl<GetAllPersons>()),
+  );
+  sl.registerFactory(
+    () => PersonSearchBloc(searchPerson: sl()),
+  );
+
+  // UseCases
+  sl.registerLazySingleton(() => GetAllPersons(sl()));
+  sl.registerLazySingleton(() => SearchPerson(sl()));
+
+  // Repository
+  sl.registerLazySingleton<PersonRepository>(
+    () => PersonRepositoryImpl(
+      remoteDataSource: sl(),
+      localDataSource: sl(),
+      networkInfo: sl(),
+    ),
+  );
+
+  sl.registerLazySingleton<PersonRemoteDataSource>(
+    () => PersonRemoteDataSourceImpl(
+      client: sl(),
+    ),
+  );
+
+  sl.registerLazySingleton<PersonLocalDataSource>(
+    () => PersonLocalDataSourceImpl(sharedPrefs: sl()),
+  );
+
+  // Core
+  sl.registerLazySingleton<NetworkInfo>(
+    () => NetworkInfoImpl(sl()),
+  );
+
+  // External
+  final sharedPreferences = await SharedPreferences.getInstance();
+  sl.registerLazySingleton(() => sharedPreferences);
+  sl.registerLazySingleton(() => http.Client());
+  sl.registerLazySingleton(() => InternetConnectionChecker());
+}
+
